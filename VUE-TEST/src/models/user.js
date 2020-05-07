28d78447d567d93ad3bccfe4a6ac84288a4b1243
mijung_kwak <mijung.kwak@bespinglobal.com>
2020-05-07 11:14:01 +0900

    vue 삭제

--- VUE-TEST/src/models/user.js
+++
@@ -1,8 +0,0 @@
-export default class User {
-    constructor(name, id, password, phonenumber) {
-        this.name = name;
-        this.id = id;
-        this.password = password;
-        this.phonenumber = phonenumber;
-    }
-}
