f9824bb1146e1156ab653c4057c00ee68e89e764
mijung_kwak <mijung.kwak@bespinglobal.com>
2020-05-07 17:47:07 +0900

    terraform 수정

--- Terraform/test.tf
+++ Terraform/test.tf
@@ -5,10 +5,19 @@ provider "aws" {
     secret_key = var.aws_secret_key
 }
 
+resource "aws_iam_instance_profile" "obd-instace_profile" {
+  name = "obd-instace_profile"
+  role = "EC2codeDeploy-kwak"
+}
+
 resource "aws_instance" "Vue" {
   ami           = var.aws_ami_id
   instance_type = "t2.micro"
   key_name = "hanju"
+
+  iam_instance_profile  {
+    arn = "${aws_iam_instance_profile.obd-instace_profile.arn}"
+  }
 
   tags = {
     Name = "obd-template-instance"
